a45430f999dd133570c57bbc2b34aa93fbb05ab3
Alexander Ivanov - Karamfil <karamfil.pc@gmail.com>
2013-02-12 18:47:58 +0200

    moved common code from atom controller to application controller

--- system/application/atoms/application_controller.php
+++ system/application/atoms/application_controller.php
@@ -2,6 +2,26 @@
 
 class Application_Controller extends library\BaseController
 {
+	public $__paths;
+	public $__javascripts			= array();
+	public $__styles				= array();
+	
+	public function __construct(library\RouterRequest $request)
+	{
+		parent::__construct($request);
+		
+		$res		= Paths_Config::$base . Dispatcher::$folder_resources . '/' . Application_Config::$folder_resources . '/';
+		$res_atom	= $res . Atom_Config::$folder_resources . '/';
+		
+		$this->__paths = array
+		(
+			'resources'		=> $res,
+			'files'			=> $res . Application_Config::$folder_files . '/',
+			'styles'		=> $res_atom . Atom_Config::$folder_styles . '/',
+			'javascripts'	=> $res_atom . Atom_Config::$folder_javascripts . '/',
+		);
+	}
+	
 	public function action__404()
 	{
 		
