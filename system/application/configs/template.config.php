209b60132b24629a7c6d1990f543c35466540614
Alexander Ivanov - Karamfil <karamfil.pc@gmail.com>
2013-02-13 11:46:01 +0200

    template security

--- system/application/configs/template.config.php
+++ system/application/configs/template.config.php
@@ -2,10 +2,43 @@
 
 final class Template_Config
 {
-	const SMARTY_FORCE_COMPILE = false;
-	const SMARTY_COMPILE_CHECK = true;
+	const COMPILE_CHECK			= true;
+	const FORCE_COMPILE			= false;
 	
-	// const SMARTY_COMPILE_HANDLER = null;
+	const ESCAPE_HTML			= true;
+	
+	// array of classes that are considered trusted. An empty array which allows access to all static classes. To disable access to all static classes set to null.
+	const SECURITY_STATIC_CLASSES		= array();
+	
+	// array of PHP functions that are considered trusted and can be used from within template. To disable access to all PHP functions set to null. An empty array will allow all PHP functions.
+	const SECURITY_PHP_FUNCTIONS		= array('isset', 'empty', 'count', 'sizeof', 'in_array', 'is_array','time','nl2br', 'de');
+	
+	// array of PHP functions that are considered trusted and can be used from within template as modifier. To disable access to all PHP modifier set to null. An empty array will allow all PHP functions.
+	const SECURITY_PHP_MODIFIERS		= array('escape','count');
+	
+	// array of streams that are considered trusted and can be used from within template. To disable access to all streams set to null. An empty array will allow all streams.
+	const SECURITY_STREAMS				= array('file');
+	
+	// array of (registered / autoloaded) modifiers that should be accessible to the template. If this array is non-empty, only the herein listed modifiers may be used. This is a whitelist.
+	const SECURITY_ALLOWED_MODIFIERS	= array();
+	
+	// array of (registered / autoloaded) modifiers that may not be accessible to the template.
+	const SECURITY_DISABLED_MODIFIERS	= array();
+	
+	// array of (registered / autoloaded) function-, block and filter plugins that should be accessible to the template. If this array is non-empty, only the herein listed function-, block and filter plugins may be used. This is a whitelist.
+	const SECURITY_ALLOWED_TAGS			= array();
+	
+	// array of (registered / autoloaded) function-, block and filter plugins that may not be accessible to the template.
+	const SECURITY_DISABLED_TAGS		= array();
+	
+	// boolean flag which controls if constants can be accessed by the template.
+	const SECURITY_ALLOW_CONSTANTS		= false;
+	
+	// boolean flag which controls if the PHP super globals can be accessed by the template.
+	const SECURITY_ALLOW_SUPER_GLOBALS	= false;
+	
+	// boolean flag which controls if {php} and {include_php} tags can be used by the template.
+	const SECURITY_ALLOW_PHP_TAG		= false;
 	
 	public static function load()
 	{
--- system/library/template/Template.php
+++ system/library/template/Template.php
@@ -17,13 +17,27 @@ final class Template extends Smarty
 		$this->template_dir		= Paths_Config::$atom_views;
 		$this->compile_dir		= Paths_Config::$atom_temp . 'tpl_compile/';
 		
-		$this->force_compile	= Template_Config::SMARTY_FORCE_COMPILE;
-		$this->compile_check	= Template_Config::SMARTY_COMPILE_CHECK;
-		$this->compile_check	= Template_Config::SMARTY_COMPILE_CHECK;
+		$this->compile_check	= Template_Config::COMPILE_CHECK;
+		$this->force_compile	= Template_Config::FORCE_COMPILE;
 		
-		$this->loadFilter(Smarty::FILTER_VARIABLE, "htmlentities");
+		$this->escape_html		= Template_Config::ESCAPE_HTML;
+		
 		// $this->caching_type		= 'memcache';
 		
+		$security_policy = new Smarty_Security($smarty);
+		$security_policy->static_classes		= Template_Config::SECURITY_STATIC_CLASSES;
+		$security_policy->php_functions			= Template_Config::SECURITY_PHP_FUNCTIONS;
+		$security_policy->php_modifiers			= Template_Config::SECURITY_PHP_MODIFIERS;
+		$security_policy->streams				= Template_Config::SECURITY_STREAMS;
+		$security_policy->allowed_modifiers		= Template_Config::SECURITY_ALLOWED_MODIFIERS;
+		$security_policy->disabled_modifiers	= Template_Config::SECURITY_DISABLED_MODIFIERS;
+		$security_policy->allowed_tags			= Template_Config::SECURITY_ALLOWED_TAGS;
+		$security_policy->disabled_tags			= Template_Config::SECURITY_DISABLED_TAGS;
+		$security_policy->allow_constants		= Template_Config::SECURITY_ALLOW_CONSTANTS;
+		$security_policy->allow_super_globals	= Template_Config::SECURITY_ALLOW_SUPER_GLOBALS;
+		$security_policy->allow_php_tag			= Template_Config::SECURITY_ALLOW_PHP_TAG;
+		
+		$this->enableSecurity($security_policy);
 		$this->addPluginsDir('./customs/');
 	}
 	
